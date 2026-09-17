1f1e581771bef9b52f7e8474d5b0d8fd2c1dab54
Aysiam223 <aysia.mercadel@sarahtreed.org>
2026-09-17 14:48:39 +0000

    .

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,21 +27,28 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     toggleGrid();
 
 
     // TODO 2 - Create Platforms
+createPlatform(300, 650, 75, 10);
+createPlatform(500, 550, 70, 10, "red");
+createPlatform(700, 450, 70, 10, "lime");
+createPlatform(900, 750, 70, 10, "lime"); 
+// bright green for a finished platform
 
 
 
 
     // TODO 3 - Create Collectables
-
+createCollectable("steve", 1350, 50);
+createCollectable("diamond", 200, 170, 0.5, 0.7);
 
 
     
     // TODO 4 - Create Cannons
-
+//createCannon("top", 200, 100);
+//createCannon("right", 300, 2000);
 
     
     
